4d37587bd8338897e2be2410f610e8c6733b1c1e
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-03 21:26:32 +0500

    First Terroform file

--- terraform1.tf
+++ terraform1.tf
@@ -14,8 +14,20 @@ variable "numberdatatype" {
 variable "boolean" {
   default =  true
 }
-variable "array"{
+
+variable "array" {
   default =  ["abc" ,123, true]
 }
 
+variable "abc" {
+  default =  {
+    string ="abc" ,
+    number =123,
+    boolean =true,
+  }
+}
+
+output "abc" {
+  value = "value1 = ${var.abc[0]},  value2 = ${var.abc[1]}"
+}
 

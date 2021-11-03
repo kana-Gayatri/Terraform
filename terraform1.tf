4bb5087d0bf34371bf4a700d6b17b9eb05d5de76
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-03 21:10:31 +0500

    First Terroform file

--- terraform1.tf
+++ terraform1.tf
@@ -1,3 +1,17 @@
 output  "abc" {
   value =  "Hello"
 }
+
+
+variable  "Stringdatatype"  {
+  default = "string"
+}
+
+variable "numberdatatype" {
+  default =  123
+}
+
+variable "boolean" {
+  default =  true
+}
+
